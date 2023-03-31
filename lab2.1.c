6720e03879ed51e37401c4282c46c85dc5d1e35a
Rosetta69 <128693855+Rosetta69@users.noreply.github.com>
2023-03-31 11:32:48 +0300

    lab2

--- lab2.1.c
+++ lab2.1.c
@@ -4,19 +4,28 @@ int main() {
     int num1, num2;
     int remainder, difference, sum;
 
+    // Запит на введення першого числа
     printf("Введіть перше число: ");
+    // Зчитування першого числа в змінну num1
     scanf("%d", &num1);
 
+    // Запит на введення другого числа
     printf("Введіть друге число: ");
+    // Зчитування другого числа в змінну num2
     scanf("%d", &num2);
 
+    // Обчислення залишку від ділення num1 на num2 і збереження у змінну remainder
     remainder = num1 % num2;
+    // Обчислення різниці між num2 і num1 і збереження у змінну difference
     difference = num2 - num1;
+    // Обчислення суми num1 і num2 і збереження у змінну sum
     sum = num1 + num2;
 
+    // Виведення результатів на екран
     printf("Залишок %d ділиться на %d Є %d\n", num1, num2, remainder);
     printf("Різниця між %d і %d є %d\n", num2, num1, difference);
     printf("Сума %d і %d є %d\n", num1, num2, sum);
 
+    // Повернення 0, що означає успішне завершення програми
     return 0;
 }
